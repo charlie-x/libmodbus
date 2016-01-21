83196ee6b7c620fc9441a6214d238d7dbcc75886
charliex <charlie@finitemonkeys.com>
2016-01-21 10:12:33 -0800

    2015 updates added X64 (untested)

--- src/modbus.h
+++ src/modbus.h
@@ -15,6 +15,7 @@
 #ifndef _MSC_VER
 #include <stdint.h>
 #else
+#pragma comment(lib,"Ws2_32.lib")
 #include "stdint.h"
 #endif
 
@@ -169,68 +170,67 @@ typedef struct {
     uint16_t *tab_registers;
 } modbus_mapping_t;
 
-typedef enum
-{
+typedef enum {
     MODBUS_ERROR_RECOVERY_NONE          = 0,
-    MODBUS_ERROR_RECOVERY_LINK          = (1<<1),
-    MODBUS_ERROR_RECOVERY_PROTOCOL      = (1<<2)
+    MODBUS_ERROR_RECOVERY_LINK          = ( 1 << 1 ),
+    MODBUS_ERROR_RECOVERY_PROTOCOL      = ( 1 << 2 )
 } modbus_error_recovery_mode;
 
-MODBUS_API int modbus_set_slave(modbus_t* ctx, int slave);
-MODBUS_API int modbus_set_error_recovery(modbus_t *ctx, modbus_error_recovery_mode error_recovery);
-MODBUS_API int modbus_set_socket(modbus_t *ctx, int s);
-MODBUS_API int modbus_get_socket(modbus_t *ctx);
+MODBUS_API int modbus_set_slave ( modbus_t* ctx, int slave );
+MODBUS_API int modbus_set_error_recovery ( modbus_t *ctx, modbus_error_recovery_mode error_recovery );
+MODBUS_API int modbus_set_socket ( modbus_t *ctx, int s );
+MODBUS_API int modbus_get_socket ( modbus_t *ctx );
 
-MODBUS_API int modbus_get_response_timeout(modbus_t *ctx, uint32_t *to_sec, uint32_t *to_usec);
-MODBUS_API int modbus_set_response_timeout(modbus_t *ctx, uint32_t to_sec, uint32_t to_usec);
+MODBUS_API int modbus_get_response_timeout ( modbus_t *ctx, uint32_t *to_sec, uint32_t *to_usec );
+MODBUS_API int modbus_set_response_timeout ( modbus_t *ctx, uint32_t to_sec, uint32_t to_usec );
 
-MODBUS_API int modbus_get_byte_timeout(modbus_t *ctx, uint32_t *to_sec, uint32_t *to_usec);
-MODBUS_API int modbus_set_byte_timeout(modbus_t *ctx, uint32_t to_sec, uint32_t to_usec);
+MODBUS_API int modbus_get_byte_timeout ( modbus_t *ctx, uint32_t *to_sec, uint32_t *to_usec );
+MODBUS_API int modbus_set_byte_timeout ( modbus_t *ctx, uint32_t to_sec, uint32_t to_usec );
 
-MODBUS_API int modbus_get_header_length(modbus_t *ctx);
+MODBUS_API int modbus_get_header_length ( modbus_t *ctx );
 
-MODBUS_API int modbus_connect(modbus_t *ctx);
-MODBUS_API void modbus_close(modbus_t *ctx);
+MODBUS_API int modbus_connect ( modbus_t *ctx );
+MODBUS_API void modbus_close ( modbus_t *ctx );
 
-MODBUS_API void modbus_free(modbus_t *ctx);
+MODBUS_API void modbus_free ( modbus_t *ctx );
 
-MODBUS_API int modbus_flush(modbus_t *ctx);
-MODBUS_API int modbus_set_debug(modbus_t *ctx, int flag);
+MODBUS_API int modbus_flush ( modbus_t *ctx );
+MODBUS_API int modbus_set_debug ( modbus_t *ctx, int flag );
 
-MODBUS_API const char *modbus_strerror(int errnum);
+MODBUS_API const char *modbus_strerror ( int errnum );
 
-MODBUS_API int modbus_read_bits(modbus_t *ctx, int addr, int nb, uint8_t *dest);
-MODBUS_API int modbus_read_input_bits(modbus_t *ctx, int addr, int nb, uint8_t *dest);
-MODBUS_API int modbus_read_registers(modbus_t *ctx, int addr, int nb, uint16_t *dest);
-MODBUS_API int modbus_read_input_registers(modbus_t *ctx, int addr, int nb, uint16_t *dest);
-MODBUS_API int modbus_write_bit(modbus_t *ctx, int coil_addr, int status);
-MODBUS_API int modbus_write_register(modbus_t *ctx, int reg_addr, int value);
-MODBUS_API int modbus_write_bits(modbus_t *ctx, int addr, int nb, const uint8_t *data);
-MODBUS_API int modbus_write_registers(modbus_t *ctx, int addr, int nb, const uint16_t *data);
-MODBUS_API int modbus_mask_write_register(modbus_t *ctx, int addr, uint16_t and_mask, uint16_t or_mask);
-MODBUS_API int modbus_write_and_read_registers(modbus_t *ctx, int write_addr, int write_nb,
-                                               const uint16_t *src, int read_addr, int read_nb,
-                                               uint16_t *dest);
-MODBUS_API int modbus_report_slave_id(modbus_t *ctx, int max_dest, uint8_t *dest);
+MODBUS_API int modbus_read_bits ( modbus_t *ctx, int addr, int nb, uint8_t *dest );
+MODBUS_API int modbus_read_input_bits ( modbus_t *ctx, int addr, int nb, uint8_t *dest );
+MODBUS_API int modbus_read_registers ( modbus_t *ctx, int addr, int nb, uint16_t *dest );
+MODBUS_API int modbus_read_input_registers ( modbus_t *ctx, int addr, int nb, uint16_t *dest );
+MODBUS_API int modbus_write_bit ( modbus_t *ctx, int coil_addr, int status );
+MODBUS_API int modbus_write_register ( modbus_t *ctx, int reg_addr, int value );
+MODBUS_API int modbus_write_bits ( modbus_t *ctx, int addr, int nb, const uint8_t *data );
+MODBUS_API int modbus_write_registers ( modbus_t *ctx, int addr, int nb, const uint16_t *data );
+MODBUS_API int modbus_mask_write_register ( modbus_t *ctx, int addr, uint16_t and_mask, uint16_t or_mask );
+MODBUS_API int modbus_write_and_read_registers ( modbus_t *ctx, int write_addr, int write_nb,
+        const uint16_t *src, int read_addr, int read_nb,
+        uint16_t *dest );
+MODBUS_API int modbus_report_slave_id ( modbus_t *ctx, int max_dest, uint8_t *dest );
 
-MODBUS_API modbus_mapping_t* modbus_mapping_offset_new(int nb_bits, int offset_bits,
-                                            int nb_input_bits, int offset_input_bits,
-                                            int nb_registers, int offset_registers,
-                                            int nb_input_registers, int offset_input_registers);
-MODBUS_API modbus_mapping_t* modbus_mapping_new(int nb_bits, int nb_input_bits,
-                                            int nb_registers, int nb_input_registers);
-MODBUS_API void modbus_mapping_free(modbus_mapping_t *mb_mapping);
+MODBUS_API modbus_mapping_t* modbus_mapping_offset_new ( int nb_bits, int offset_bits,
+        int nb_input_bits, int offset_input_bits,
+        int nb_registers, int offset_registers,
+        int nb_input_registers, int offset_input_registers );
+MODBUS_API modbus_mapping_t* modbus_mapping_new ( int nb_bits, int nb_input_bits,
+        int nb_registers, int nb_input_registers );
+MODBUS_API void modbus_mapping_free ( modbus_mapping_t *mb_mapping );
 
-MODBUS_API int modbus_send_raw_request(modbus_t *ctx, uint8_t *raw_req, int raw_req_length);
+MODBUS_API int modbus_send_raw_request ( modbus_t *ctx, uint8_t *raw_req, int raw_req_length );
 
-MODBUS_API int modbus_receive(modbus_t *ctx, uint8_t *req);
+MODBUS_API int modbus_receive ( modbus_t *ctx, uint8_t *req );
 
-MODBUS_API int modbus_receive_confirmation(modbus_t *ctx, uint8_t *rsp);
+MODBUS_API int modbus_receive_confirmation ( modbus_t *ctx, uint8_t *rsp );
 
-MODBUS_API int modbus_reply(modbus_t *ctx, const uint8_t *req,
-                            int req_length, modbus_mapping_t *mb_mapping);
-MODBUS_API int modbus_reply_exception(modbus_t *ctx, const uint8_t *req,
-                                      unsigned int exception_code);
+MODBUS_API int modbus_reply ( modbus_t *ctx, const uint8_t *req,
+                              int req_length, modbus_mapping_t *mb_mapping );
+MODBUS_API int modbus_reply_exception ( modbus_t *ctx, const uint8_t *req,
+                                        unsigned int exception_code );
 
 /**
  * UTILS FUNCTIONS
@@ -263,21 +263,21 @@ MODBUS_API int modbus_reply_exception(modbus_t *ctx, const uint8_t *req,
         tab_int16[(index) + 3] = (value); \
     } while (0)
 
-MODBUS_API void modbus_set_bits_from_byte(uint8_t *dest, int idx, const uint8_t value);
-MODBUS_API void modbus_set_bits_from_bytes(uint8_t *dest, int idx, unsigned int nb_bits,
-                                       const uint8_t *tab_byte);
-MODBUS_API uint8_t modbus_get_byte_from_bits(const uint8_t *src, int idx, unsigned int nb_bits);
-MODBUS_API float modbus_get_float(const uint16_t *src);
-MODBUS_API float modbus_get_float_abcd(const uint16_t *src);
-MODBUS_API float modbus_get_float_dcba(const uint16_t *src);
-MODBUS_API float modbus_get_float_badc(const uint16_t *src);
-MODBUS_API float modbus_get_float_cdab(const uint16_t *src);
+MODBUS_API void modbus_set_bits_from_byte ( uint8_t *dest, int idx, const uint8_t value );
+MODBUS_API void modbus_set_bits_from_bytes ( uint8_t *dest, int idx, unsigned int nb_bits,
+        const uint8_t *tab_byte );
+MODBUS_API uint8_t modbus_get_byte_from_bits ( const uint8_t *src, int idx, unsigned int nb_bits );
+MODBUS_API float modbus_get_float ( const uint16_t *src );
+MODBUS_API float modbus_get_float_abcd ( const uint16_t *src );
+MODBUS_API float modbus_get_float_dcba ( const uint16_t *src );
+MODBUS_API float modbus_get_float_badc ( const uint16_t *src );
+MODBUS_API float modbus_get_float_cdab ( const uint16_t *src );
 
-MODBUS_API void modbus_set_float(float f, uint16_t *dest);
-MODBUS_API void modbus_set_float_abcd(float f, uint16_t *dest);
-MODBUS_API void modbus_set_float_dcba(float f, uint16_t *dest);
-MODBUS_API void modbus_set_float_badc(float f, uint16_t *dest);
-MODBUS_API void modbus_set_float_cdab(float f, uint16_t *dest);
+MODBUS_API void modbus_set_float ( float f, uint16_t *dest );
+MODBUS_API void modbus_set_float_abcd ( float f, uint16_t *dest );
+MODBUS_API void modbus_set_float_dcba ( float f, uint16_t *dest );
+MODBUS_API void modbus_set_float_badc ( float f, uint16_t *dest );
+MODBUS_API void modbus_set_float_cdab ( float f, uint16_t *dest );
 
 #include "modbus-tcp.h"
 #include "modbus-rtu.h"
